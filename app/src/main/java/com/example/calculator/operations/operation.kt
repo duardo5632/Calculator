86506b3dda0d03e35d3caf359f057c7f32e0faf1
eduardo <aris.duardo5632@gmail.com>
2024-01-22 20:19:44 -0400

    primer calculo

--- app/src/main/java/com/example/calculator/operations/operation.kt
+++ app/src/main/java/com/example/calculator/operations/operation.kt
@@ -1,14 +1,22 @@
 package com.example.calculator.operations
 
+import kotlin.time.times
+
 //no detecta los signos
 //ver quien almacena las operaciones aritmeticas
-fun evaluateExpression(expre: String):Double {
+fun evaluateExpression(expre: String):Number {
 
-    val tokens = expre.split(" ")
+    //val tokens = expre.split(" ")
 
+    val regex = Regex("""([\d.]+|[+\-x÷])""")
+    val tokens = regex.findAll(expre).map { it.value }.toList()
+
+    //donde se muestra la respuesta del calculo
     var result = tokens[0].toDoubleOrNull() ?: 0.0
 
     for (i in 1 until  tokens.size step  2){
+
+        //ver si puedo quitar la linea de codigo operator
         val operator = tokens[i]
         val operand = tokens[i + 1].toDoubleOrNull()?:0.0
 
@@ -20,5 +28,7 @@ fun evaluateExpression(expre: String):Double {
             else -> result
         }
     }
-    return result
+    //condicion que dice que si el reultado no tiene indice y
+    // es un numero entero entonces que la respuesta sea un numero entero y no un 0.0
+    return if (result % 1 == 0.0) result.toInt() else result
 }
